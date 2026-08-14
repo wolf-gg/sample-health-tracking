b51d3e7e0b453069f1e5069a62d1c7da4f0af2a6
Johann Exconde <johann.exconde@gmail.com>
2026-08-15 00:20:53 +0800

    feat(server): add router folder
    This will serve as the main folder for all controllers. This will be
    considered as the first part of making the server app follow the CSR
    pattern. We will introduce `service` and `repository` folders later on
    once we add in the database driver.

--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -2,6 +2,7 @@ import bodyParser from "body-parser";
 import express, { type Express } from "express";
 import morgan from "morgan";
 import cors from "cors";
+import healthRouter from "./router/health";
 
 const { json, urlencoded } = bodyParser;
 
@@ -13,12 +14,7 @@ export const createServer = (): Express => {
     .use(urlencoded({ extended: true }))
     .use(json())
     .use(cors())
-    .get("/message/:name", (req, res) => {
-      return res.json({ message: `hello ${req.params.name}` });
-    })
-    .get("/status", (_, res) => {
-      return res.json({ ok: true });
-    });
+    .use("/health", healthRouter);
 
   return app;
 };
